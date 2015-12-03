a0657ed49d851cec647257fd992be6740fc11f7a
Rei Matsuzaki <matsuzaki.rei@gmail.com>
2015-12-03 17:09:49 +0900

    define add and mult

--- py_bind/l2func.py
+++ py_bind/l2func.py
@@ -1,14 +1,28 @@
 from l2func_bind import *
 
-class OpAdd:
+class Add:
     def __init__(self, a, b):
         self.left = a
         self.right = b
 
-class OpMult:
+class Mult:
     def __init__(self, a, b):
         self.left = a
         self.right = b
+
+#class OpFunc:
+#    def __init__(self, a, b):
+#        self.left = a
+#        self.right = b
+
+def add(self, other):
+    return Add(self, other)
+
+def mult(self, other):
+    return Mult(self, other)
+
+STO.__add__ = add
+GTO.__mul__ = add
 
 cip_dict = {}
 cip_dict[(STO, STO)] = cip_ss
@@ -38,16 +52,35 @@ cip_op_dict[(GTO, OpRm, GTO)] = cip_g_rm_g
 def get_cip_op(a, o, b):
     return cip_op_dict[(type(a), type(o), type(b))]
 
+def cip_ab(a, b):
+    if(isinstance(a, Add)):
+        return cip_ab(a.left, b) + cip_ab(a.right, b)
+    if(isinstance(a, Mult)):
+        return a.left * cip_ab(a.right, b)
+    if(isinstance(b, Add)):
+        return cip_ab(a, b.left) + cip_ab(a, b.right)
+    if(isinstance(b, Mult)):
+        return b.left * cip_ab(a, b.left)
+    return get_cip(a, b)(a, b)
+
 def cip_a_op_b(a, o, b):
-    if(isinstance(o, OpAdd)):
+    if(isinstance(a, Add)):
+        return cip_a_op_b(a.left, o, b) + cip_a_op_b(a.right, o, b)
+    if(isinstance(a, Mult)):
+        return a.left * cip_a_op_b(a.right, o, b)
+    if(isinstance(b, Add)):
+        return cip_a_op_b(a, o, b.left) + cip_a_op_b(a, o, b.right)
+    if(isinstance(b, Mult)):
+        return b.left * cip_a_op_b(a, o, b.left)
+    if(isinstance(o, Add)):
         return cip_a_op_b(a, o.left, b) + cip_a_op_b(a, o.right, b)
-    if(isinstance(o, OpMult)):
+    if(isinstance(o, Mult)):
         return o.left * cip_a_op_b(a, o.right, b)
     return get_cip_op(a, o, b)(a, o, b)
 
 def cip(a, b, c=None):
     if(c == None):
-        return get_cip(a, b)(a, b)
+        return cip_ab(a, b)
     else:
         return cip_a_op_b(a, b, c)
 
