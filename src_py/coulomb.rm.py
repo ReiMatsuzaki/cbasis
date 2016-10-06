cfc8e16be3feb23f37969b8e11ad47c3ae9fddf6
Rei Matsuzaki <matsuzaki.rei@gmail.com>
2016-10-06 13:15:27 +0900

    add src_py/coulomb.rm.py

--- src_py/coulomb.rm.py
+++ src_py/coulomb.rm.py
@@ -1,13 +1,2 @@
-import sympy
-import cmath
+## this files is add to opt_gto/coulomb_func/coulomb.py
 
-def coulomb_phase(L, eta):
-    """
-    Gives the Coulomb phase shift.
-    -------
-    \sigma_L = arg Gamma(L+1+i\eta)
-    \eta = z1 z2 / k
-    -------
-    """
-    return cmath.phase(sympy.gamma(1+L+1.0j*eta))
-    
